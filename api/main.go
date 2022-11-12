180d0a7e9e4a5b5a2346ba76be7bbee3c45e29ad
Steven Gao <stevenruidigao@gmail.com>
2022-11-12 09:46:12 +0000

    working?

--- api/main.go
+++ api/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-	// "context"
+	"context"
 	"crypto/rand"
 	"encoding/hex"
+	"encoding/json"
+	"log"
 
 	// "encoding/json"
 	// "flag"
@@ -11,13 +13,29 @@ import (
 	// "io/ioutil"
 	"net/http"
 	// "strconv"
-	// "strings"
-	// "time"
+	"strings"
+	"time"
 
 	"github.com/go-redis/redis/v8"
 	"github.com/gorilla/mux"
+	"github.com/igm/pubsub"
+	"github.com/igm/sockjs-go/v3/sockjs"
 	"github.com/spf13/viper"
 )
+
+type Details struct {
+	Location     string    `json:"dp:location"`
+	LocationName string    `json:"dp:locationName"`
+	Address      string    `json:"address"`
+	Loads        []float64 `json:"load"`
+	Time         time.Time `json:"time"`
+	Success      bool      `json:"success"`
+	Message      string    `json:"message"`
+}
+
+var ctx = context.Background()
+var publisher pubsub.Publisher
+var redisClient *redis.Client
 
 func main() {
 	viper.SetConfigFile(".env")
@@ -42,7 +60,7 @@ func main() {
 	redisHost := viper.GetString("REDIS_HOST")
 	redisPort := viper.GetString("REDIS_PORT")
 	redisPassword := viper.GetString("REDIS_PASSWORD")
-	redisDB := viper.GetString("REDIS_DB")
+	redisDB := viper.GetInt("REDIS_DB")
 	cacheToken := viper.GetString("CACHE_TOKEN")
 	sessionKey := viper.GetString("SESSION_KEY")
 	// cookieMaxAge := viper.GetInt("COOKIE_MAX_AGE")
@@ -51,7 +69,7 @@ func main() {
 	if cacheToken == "" {
 		bytes := make([]byte, 4)
 		rand.Read(bytes)
-		cacheToken = hex.EncodeToString(bytes)
+		// cacheToken = hex.EncodeToString(bytes)
 	}
 
 	if sessionKey == "" {
@@ -64,14 +82,77 @@ func main() {
 		viper.WriteConfig()
 	}
 
-	rdb := redis.NewClient(&redis.Options{
+	redisClient = redis.NewClient(&redis.Options{
 		Addr:     redisHost + ":" + redisPort,
 		Password: redisPassword,
 		DB:       redisDB,
 	})
 
 	router := mux.NewRouter()
+	apiRouter := router.PathPrefix("/api").Subrouter()
+	v1Router := apiRouter.PathPrefix("/v1").Subrouter()
+
+	v1Router.HandleFunc("/details/{location}", func(writer http.ResponseWriter, request *http.Request) {
+		location := mux.Vars(request)["dp:location"]
+		fmt.Println("Get details of", location)
+		details := []Details{{Location: location, Message: "Success.", Time: time.Now()}}
+		loadString, err := redisClient.Get(ctx, "dp:location:"+location+":loads").Result()
+
+		if err != nil {
+			if err == redis.Nil {
+				details[0].Message = "That location does not exist."
+
+			} else {
+				details[0].Message = "An unknown error occurred."
+			}
+
+			details[0].Success = false
+			json.NewEncoder(writer).Encode(details)
+			return
+		}
+
+		details[0].Loads = []float64{}
+		err = json.Unmarshal([]byte(loadString), &details[0].Loads)
+
+		if err != nil {
+			details[0].Message = "An unknown error occurred."
+			details[0].Success = false
+			json.NewEncoder(writer).Encode(details)
+			return
+		}
+
+		locationName, err := redisClient.Get(ctx, "dp:location:"+location+":name").Result()
+
+		if err != nil {
+			if err == redis.Nil {
+				details[0].Message = "That location does not exist."
+
+			} else {
+				details[0].Message = "An unknown error occurred."
+			}
+
+			details[0].Success = false
+			json.NewEncoder(writer).Encode(details)
+			return
+		}
+
+		details[0].LocationName = locationName
+		json.NewEncoder(writer).Encode(details)
+	})
+
+	v1Router.HandleFunc("/locations", func(writer http.ResponseWriter, request *http.Request) {
+		locations, err := redisClient.SMembers(ctx, "dp:locations").Result()
+		fmt.Println("Get locations")
+
+		if err != nil {
+			fmt.Println("An error occurred while getting the list of available locations:", err)
+		}
+
+		json.NewEncoder(writer).Encode(locations)
+	})
+
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./public")))
+	http.Handle("/socket/", sockjs.NewHandler("/socket", sockjs.DefaultOptions, connectionHandler))
 	fmt.Println("Listening on " + host + ":" + port)
 	http.ListenAndServe(host+":"+port, router)
 
@@ -96,4 +177,154 @@ func main() {
 		        fmt.Println("key2", val2)
 		    }
 	*/
+}
+
+func Update() {
+	locations, err := redisClient.SMembers(ctx, "dp:locations").Result()
+
+	if err != nil {
+		fmt.Println("An error occurred while getting the list of available locations:", err)
+	}
+
+	details := make([]Details, len(locations))
+
+	for i, location := range locations {
+		details[i] = Details{Location: location, Message: "Success.", Time: time.Now()}
+
+		loadString, err := redisClient.Get(ctx, "dp:location:"+location+":loads").Result()
+
+		if err != nil {
+			fmt.Println("An error occurred:", err)
+		}
+
+		details[i].Loads = []float64{}
+		err = json.Unmarshal([]byte(loadString), &details[i].Loads)
+
+		if err != nil {
+			fmt.Println("An error occurred:", err)
+		}
+
+		locationName, err := redisClient.Get(ctx, "dp:location:"+location+":name").Result()
+
+		if err != nil {
+			fmt.Println("An error occurred:", err)
+		}
+
+		details[i].LocationName = locationName
+	}
+
+	rawUpdate, _ := json.Marshal(details)
+	publisher.Publish(string(rawUpdate))
+	time.AfterFunc(30*time.Second, Update)
+}
+
+func connectionHandler(session sockjs.Session) {
+	fmt.Println("New SockJS session established.")
+	closedSession := make(chan struct{})
+	// publisher.Publish("[info] new participant joined chat")
+	// defer publisher.Publish("[info] participant left chat")
+	go func() {
+		reader, _ := publisher.SubChannel(nil)
+
+		for {
+			select {
+			case <-closedSession:
+				return
+
+			case msg := <-reader:
+				if err := session.Send(msg.(string)); err != nil {
+					return
+				}
+			}
+		}
+	}()
+
+	for {
+		if msg, err := session.Recv(); err == nil {
+			details := []Details{{Message: "Success.", Success: true, Time: time.Now()}}
+
+			if strings.Contains(msg, "dp:location: ") {
+				messageParts := strings.Split(msg, "dp:location: ")
+
+				if len(messageParts) < 2 {
+					details[0].Message = "Malformed request."
+					details[0].Success = false
+					rawResponse, _ := json.Marshal(details)
+
+					if err := session.Send(string(rawResponse)); err != nil {
+						break
+					}
+				}
+
+				location := strings.Split(msg, "dp:location: ")[1]
+				details[0].Location = location
+				loadString, err := redisClient.Get(ctx, "dp:location:"+location+":loads").Result()
+
+				if err != nil {
+					if err == redis.Nil {
+						details[0].Message = "That location does not exist."
+
+					} else {
+						details[0].Message = "An unknown error occurred."
+					}
+
+					details[0].Success = false
+					rawResponse, _ := json.Marshal(details)
+
+					if err := session.Send(string(rawResponse)); err != nil {
+						break
+					}
+
+					return
+				}
+
+				details[0].Loads = []float64{}
+				err = json.Unmarshal([]byte(loadString), &details[0].Loads)
+
+				if err != nil {
+					details[0].Message = "An unknown error occurred."
+					details[0].Success = false
+					rawResponse, _ := json.Marshal(details)
+
+					if err := session.Send(string(rawResponse)); err != nil {
+						break
+					}
+
+					return
+				}
+
+				locationName, err := redisClient.Get(ctx, "dp:location:"+location+":name").Result()
+
+				if err != nil {
+					if err == redis.Nil {
+						details[0].Message = "That location does not exist."
+
+					} else {
+						details[0].Message = "An unknown error occurred."
+					}
+
+					details[0].Success = false
+					rawResponse, _ := json.Marshal(details)
+
+					if err := session.Send(string(rawResponse)); err != nil {
+						break
+					}
+
+					return
+				}
+
+				details[0].LocationName = locationName
+				rawResponse, _ := json.Marshal(details)
+
+				if err := session.Send(string(rawResponse)); err != nil {
+					break
+				}
+			}
+			continue
+		}
+		break
+	}
+
+	close(closedSession)
+	log.Println("SockJS session closed.")
 }
--- api/run.sh
+++ api/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gofmt -w *.go && go build && ./umassdiningplus.tech
+gofmt -w *.go && go get && go build && ./umassdiningplus.tech
